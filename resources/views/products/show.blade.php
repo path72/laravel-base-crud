1253e90f3969e69b07c16f205b5bce97d24360c7
Salvatore Trimarchi <path72@gmail.com>
2021-05-13 18:35:34 +0200

    detail page css fix?

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.app')
-{{-- @section('css')
-	<link rel="stylesheet" href="css/app.css">
-@endsection --}}
+@section('css')
+	<link rel="stylesheet" href="../css/app.css">
+@endsection
 
 @section('title','Product Page')
 @section('main_content')
 
+<main>
+	<h3>Product {{$product['model']}}</h3>
+
 	<div class="card">
-		<h3>Product {{$product['model']}} (id: {{$product['id']}})</h3>
-	
+		<p>id: {{$product['id']}}</p>
 		<p>Size: {{$product['size']}}</p>
 		<p>Color: {{$product['color']}}</p>
 		<p>Fabric: {{$product['fabric']}}</p>
 		<p>Stock: {{$product['stock']}}</p>
-	
 		@php
 			if ($product['availability']==1) $msg = 'Yes';
 			else $msg = 'No';
 		@endphp
 		<p>Available: {{$msg}}</p>
 	</div>
+</main>
 
 @endsection
